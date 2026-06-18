4677e3baa24ca24d59754fd456114c41fc68aea5
KARUNATHILAKE R.T.N.K <IT25103329@my.sliit.lk>
2026-06-18 20:43:04 +0530

    Created the Medical Record Controller.

--- src/main/java/com/hospitalmanagementsystem/controllers/MedicalRecordController.java
+++ src/main/java/com/hospitalmanagementsystem/controllers/MedicalRecordController.java
@@ -1,4 +1,4 @@
-package com.hospitalmanagementsystem.controllers;
+ppackage com.hospitalmanagementsystem.controllers;
 
 import com.hospitalmanagementsystem.models.Appointment;
 import com.hospitalmanagementsystem.models.GeneralMedicalRecord;
@@ -10,10 +10,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
